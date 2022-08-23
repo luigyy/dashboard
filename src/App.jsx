340a5d0945c214c41754f0722c1a85c744b56086
luigyy <luigy.valverde@ucr.ac.cr>
2022-08-23 17:21:43 -0600

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import React, { useEffect } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 import "./App.css";
-
+import {Route, BrowserRouter, Routes} from 'react-router-dom' 
 import { Navbar, Footer, Sidebar, ThemeSettings } from "./components"
 
 import {Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Line,
