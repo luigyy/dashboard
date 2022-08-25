b3618136e3bda46db18e504ca440419a643f38b6
luigyy <luigy.valverde@ucr.ac.cr>
2022-08-24 22:25:09 -0600

    working on ecommerce main

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -28,29 +28,73 @@ const Ecommerce = () => {
         </div>
         {/**main card */}
 
-        {/**Cards*/}
-        <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
-          {earningData.map((item) => (
-            <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg md:w-66 p-4 pt-9 rounded-2xl">
-              <button
-                type="button"
-                style={{ color: item.iconColor, backgroundColor: item.iconBg }}
-              >
-                {item.icon}
-              </button>
-              <p className="mt-3">
-                <span className="text-lg font-semibold">{item.amount}</span>
-                <span className={`text-sm text-${item.pcColor} ml-2`}>{item.percentage}</span>
-              </p>
-              <p className="text-sm text-gray-400 mt-1">{item.title}</p>
-            </div>
-          ))}
-        </div>
-        {/**Cards */}
+      {/**Cards*/}
+      <div className="flex m-3 flex-wrap justify-center gap-5 items-center">
+        {earningData.map((item) => (
+          <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg md:w-66 p-4 pt-9 rounded-2xl">
+            <button
+              type="button"
+              style={{ color: item.iconColor, backgroundColor: item.iconBg }}
+            >
+              {item.icon}
+            </button>
+            <p className="mt-3">
+              <span className="text-lg font-semibold">{item.amount}</span>
+              <span className={`text-sm text-${item.pcColor} ml-2`}>
+                {item.percentage}
+              </span>
+            </p>
+            <p className="text-sm text-gray-400 mt-1">{item.title}</p>
+          </div>
+        ))}
       </div>
+      </div>
+      {/**Cards */}
       {/**Revenue updates*/}
-      <div>
-
+      <div className="flex gap-10 flex-wrap justify-center">
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780">
+          <div className="flex justify-between">
+            <p className="font-semibold text-xl">Revenue updates</p>
+            <div className="flex items-center gap-4">
+              <p className="flex items-center gap-4 text-gray-600 hover:drop-shadow-xl">
+                <span>
+                  <GoPrimitiveDot />
+                </span>
+                <span>Expense</span>
+              </p>
+              <p className="flex items-center gap-4 text-green-600 hover:drop-shadow-xl">
+                <span>
+                  <GoPrimitiveDot />
+                </span>
+                <span>Expense</span>
+              </p>
+            </div>
+          </div>
+          <div className="mt-10 flex gap-10 flex-wrap justify-center">
+            {/**card left */}
+            <div className="border-r-1 border-color m-4 pr-10">
+              <div>
+                <p>
+                  <span className="text-xl font-semibold">$91,890.00</span>
+                  <span className="p-1.5 hover:drop-shadow-2xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs">
+                    23%
+                  </span>
+                </p>
+                <p className="text-gray-500 mt-1">Budget</p>
+              </div>
+              <div className="mt-8">
+                <p>
+                  <span className="text-xl font-semibold">$18,890.00</span>
+                </p>
+                <p className="text-gray-500 mt-1">Expense</p>
+              </div>
+            <div className="mt-5">
+              <SparkLine/>
+            </div>
+            </div>
+            {/**card left */}
+          </div>
+        </div>
       </div>
       {/**Revenue updates*/}
     </div>
